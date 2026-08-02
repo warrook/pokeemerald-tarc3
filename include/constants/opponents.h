18cc86c3de9ba20ce762773413a381fb0f57be38
warrook <rook.kingsley@gmail.com>
2026-08-02 13:38:33 -0700

    Prelim trainer tests

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -860,6 +860,8 @@
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
 
+#define TRAINER_BEACH_A                     1
+
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
