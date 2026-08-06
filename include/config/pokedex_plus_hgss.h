e73a1a90f5716eea4485f21e4d11c9f1579ca6b3
warrook <rook.kingsley@gmail.com>
2026-08-06 12:18:32 -0700

    Expansion configs

--- include/config/pokedex_plus_hgss.h
+++ include/config/pokedex_plus_hgss.h
@@ -4,7 +4,7 @@
 #define POKEDEX_PLUS_HGSS                    TRUE // If TRUE, enables the custom HGSS style Pokedex.
 #define HGSS_DECAPPED                       FALSE // If TRUE, uses decapped gfx and strings.
 #define HGSS_DARK_MODE                      FALSE // If TRUE, enables dark mode.
-#define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES    FALSE // If TRUE, hides evolution mon names.
+#define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES     TRUE // If TRUE, hides evolution mon names.
 #define HGSS_HIDE_UNOWNED_EVOLUTION_METHODS FALSE // If TRUE, hides evolution methods.
 #define HGSS_SORT_TMS_BY_NUM                FALSE // If TRUE, sorts the TMS in HGSS Dex by TM number, rather than alphabetically.
 #define HGSS_SHOW_EGG_MOVES_FOR_EVOS        FALSE // If TRUE, shows Egg Moves for evolved Pokémon too.
--- src/quickstart.c
+++ src/quickstart.c
@@ -79,7 +79,7 @@ static void CB2_SkipToNewGame(void)
     static const u8 sText_PlayerFemale[] = _("LEAF");
     static const u8 sText_Rival[] = _("BLUE");
 #else
-    static const u8 sText_PlayerMale[] = _("BRENDAN");
+    static const u8 sText_PlayerMale[] = _("Robin");
     static const u8 sText_PlayerFemale[] = _("MAY");
 #endif  // IS_FRLG
 
