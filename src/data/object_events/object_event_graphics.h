0b48b3f31da80424c9852e19881c7a800bf577ee
warrook <rook.kingsley@gmail.com>
2026-09-07 23:49:51 -0700

    Re-add palookaport palette
    oops

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -7,6 +7,7 @@ const u16 gObjectEventPal_Palooka[] = INCGFX_U16("graphics/object_events/pics/pe
 const u32 gObjectEventPic_PalookaBoss[] = INCGFX_U32("graphics/object_events/pics/people/palooka_boss.png", ".4bpp", "-mwidth 2 -mheight 4");
 const u16 gObjectEventPal_PalookaBoss[] = INCGFX_U16("graphics/object_events/pics/people/palooka_boss.png", ".gbapal");
 const u32 gFieldEffectObjectPic_Palookaport[] = INCGFX_U32("graphics/field_effects/pics/palookaport.png", ".4bpp", "-mwidth 2 -mheight 4");
+const u16 gFieldEffectPal_Palookaport[] = INCGFX_U16("graphics/field_effects/pics/palookaport.png", ".gbapal");
 
 const u32 gObjectEventPic_BrendanNormalRunning[] = INCBIN_U32("graphics/object_events/pics/people/brendan/walking.4bpp", "graphics/object_events/pics/people/brendan/running.4bpp");
 const u16 gObjectEventPal_Brendan[] = INCGFX_U16("graphics/object_events/pics/people/brendan/walking.png", ".gbapal");
